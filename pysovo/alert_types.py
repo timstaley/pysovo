2c7efce0268ae88dcc68493dcfab133136ddd7c7
Tim Staley <tim.staley@soton.ac.uk>
2012-10-12 15:13:04 +0100

    Add new test file

--- pysovo/alert_types.py
+++ pysovo/alert_types.py
@@ -1,4 +1,2 @@
-#Normally I would capitalise this i.e. AlertTypes
-#But since we're using it as a namespace and not a class, this seems more fitting.
 
-swift_grb="swift_grb"
+swift_grb = "swift_grb"
